fa33b939ad3dd3bbcf16c3ebbfc1e43d0da8e076
Anuraag Agrawal <aanuraag@amazon.co.jp>
2020-08-17 12:54:51 +0900

    Implement subscription for outputstreamrequest

--- src/main/java/com/github/burrunan/s3cache/internal/awssdk/OutputStreamRequest.java
+++ src/main/java/com/github/burrunan/s3cache/internal/awssdk/OutputStreamRequest.java
@@ -4,23 +4,30 @@ import java.io.IOException;
 import java.io.OutputStream;
 import java.nio.ByteBuffer;
 import java.util.Optional;
-import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.atomic.AtomicLong;
 
 import org.jetbrains.annotations.NotNull;
 import org.reactivestreams.Subscriber;
+import org.reactivestreams.Subscription;
 
 import software.amazon.awssdk.core.async.AsyncRequestBody;
 
 public final class OutputStreamRequest extends OutputStream implements AsyncRequestBody {
 
     private final long contentLength;
-    private final CountDownLatch subscribed;
+    private final AtomicLong demand;
+    private final BlockingQueue<String> barrier;
 
     private Subscriber<? super ByteBuffer> subscriber;
 
+    private volatile boolean cancelled;
+
     public OutputStreamRequest(long contentLength) {
         this.contentLength = contentLength;
-        subscribed = new CountDownLatch(1);
+        demand = new AtomicLong();
+        barrier = new ArrayBlockingQueue<>(1);
     }
 
     @Override
@@ -31,18 +38,18 @@ public final class OutputStreamRequest extends OutputStream implements AsyncRequ
     @Override
     public void subscribe(Subscriber<? super ByteBuffer> s) {
         subscriber = s;
-        subscribed.countDown();
+        s.onSubscribe(new OutputStreamSubscription());
     }
 
     @Override
     public void write(int b) throws IOException {
-        waitForSubscriber();
+        waitForDemand();
         subscriber.onNext(ByteBuffer.wrap(new byte[] { (byte) b }));
     }
 
     @Override
     public void write(@NotNull byte[] b, int off, int len) throws IOException {
-        waitForSubscriber();
+        waitForDemand();
         subscriber.onNext(ByteBuffer.wrap(b, off, len));
     }
 
@@ -51,11 +58,30 @@ public final class OutputStreamRequest extends OutputStream implements AsyncRequ
         subscriber.onComplete();
     }
 
-    private void waitForSubscriber() throws IOException {
-        try {
-            subscribed.await();
-        } catch (InterruptedException e) {
-            throw new IOException("Interrupted while waiting for async subscriber", e);
+    private void waitForDemand() throws IOException {
+        if (cancelled) {
+            throw new IOException("Stream cancelled during write.");
+        }
+        if (demand.get() == 0) {
+            try {
+                barrier.take();
+            } catch (InterruptedException e) {
+                throw new IOException("Interrupted while waiting for async subscriber", e);
+            }
+        }
+        demand.decrementAndGet();
+    }
+
+    private class OutputStreamSubscription implements Subscription {
+        @Override
+        public void request(long n) {
+            demand.addAndGet(n);
+            barrier.offer("");
+        }
+
+        @Override
+        public void cancel() {
+            cancelled = true;
         }
     }
 }
